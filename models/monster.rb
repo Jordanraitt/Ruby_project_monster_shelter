abc1977a698a7d41beca20b114156d1241b9eafe
jordanraitt <jordanraitt@hotmail.com>
2018-10-05 16:57:50 +0100

    CRUD comnpleted

--- models/monster.rb
+++ models/monster.rb
@@ -24,4 +24,46 @@ attr_reader :id, :name
     @id = result.first()['id'].to_i
   end
 
+  def update()
+      sql = "UPDATE monsters
+      SET
+      (
+        name
+
+      ) =
+      (
+        $1
+      )
+      WHERE id = $2"
+      values = [@name, @id]
+      SqlRunner.run( sql, values )
+    end
+
+  def self.delete_all()
+    sql = "DELETE FROM monsters;"
+    SqlRunner.run(sql)
+  end
+
+  def delete()
+    sql = "DELETE FROM monsters
+    WHERE id = $1"
+    values = [@id]
+    SqlRunner.run( sql, values )
+  end
+
+  def self.all()
+    sql = "SELECT * FROM monsters"
+    monster = SqlRunner.run( sql )
+    result = monster.map { |monster| Monster.new( monster ) }
+    return result
+  end
+
+  def self.find( id )
+    sql = "SELECT * FROM monsters WHERE id = $1"
+    values = [id]
+    monster = SqlRunner.run( sql, values )
+    result = Monster.new( monster.first )
+    return result
+  end
+
 end
--- models/trainer.rb
+++ models/trainer.rb
@@ -24,4 +24,46 @@ def save()
   @id = result.first()['id'].to_i
 end
 
+def update()
+    sql = "UPDATE trainers
+    SET
+    (
+      name
+
+    ) =
+    (
+      $1
+    )
+    WHERE id = $2"
+    values = [@name, @id]
+    SqlRunner.run( sql, values )
+  end
+
+  def self.delete_all()
+    sql = "DELETE FROM trainers;"
+    SqlRunner.run(sql)
+  end
+
+  def delete()
+    sql = "DELETE FROM trainers
+    WHERE id = $1"
+    values = [@id]
+    SqlRunner.run( sql, values )
+  end
+
+  def self.all()
+    sql = "SELECT * FROM trainers"
+    trainer = SqlRunner.run( sql )
+    result = trainer.map { |trainer| Trainer.new( trainer ) }
+    return result
+  end
+
+  def self.find( id )
+    sql = "SELECT * FROM trainers WHERE id = $1"
+    values = [id]
+    trainer = SqlRunner.run( sql, values )
+    result = Trainer.new( trainer.first )
+    return result
+  end
+
 end
